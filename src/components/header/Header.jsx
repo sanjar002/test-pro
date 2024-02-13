86e3988a98356c10912e5e32e8c97e0b20e2568e
Jakyp-05 <jakypjumagulov080gmail.com>
2024-02-13 15:07:40 +0600

    header router

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import './Header.scss'
 
+import logo from '../../assets/image/okurmen-logo .png'
+import { Link } from 'react-router-dom'
+
 const Header = () => {
-  return (
-    <div>
-      Header
-    </div>
-  )
+	return (
+		<header className='container'>
+			<div className='logo'>
+				<img src={logo} alt='okurmen logo' />
+			</div>
+			<nav>
+				<ul>
+					<li>
+						<Link to='/'>Главная</Link>
+					</li>
+					<li>
+						<Link to='/курсы'>Курсы</Link>
+					</li>
+					<li>
+						<Link to='/преподавател'>Преподавател</Link>
+					</li>
+					<li>
+						<Link to='/о нас'>О нас</Link>
+					</li>
+					<li>
+						<Link to='/отзывы'>Отзывы</Link>
+					</li>
+					<li>
+						<Link to='Контакты'>Контакты</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	)
 }
 
 export default Header
