09e830337b21e11d62cb9c25f403f28c89368a14
Jakyp-05 <jakypjumagulov080gmail.com>
2024-02-13 14:07:28 +0600

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './index.scss'
+import Layout from './components/Layout/Layout'
 const App = () => {
-  return (
-    <div>
-      app
-    </div>
-  )
+	return (
+		<div>
+			<Routes>
+				<Route path='/' element={<Layout />}></Route>
+			</Routes>
+		</div>
+	)
 }
 
 export default App
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Header from '../header/Header'
+import { Outlet } from 'react-router-dom'
+import Footer from '../footer/Footer'
 
 const Layout = () => {
-  return (
-    <div>
-      Layout
-    </div>
-  )
+	return (
+		<div>
+			<Header />
+			<Outlet />
+			<Footer />
+		</div>
+	)
 }
 
 export default Layout
